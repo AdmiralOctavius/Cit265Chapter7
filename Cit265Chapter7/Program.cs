fa32355cafc2a4286e9d84c844101680edd05e78
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-09-05 13:10:46 -0400

    Test2

---
+++ Cit265Chapter7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cit265Chapter7
+{
+    class Program
+    {
+        private enum Colors
+        {
+            Red=0, Green=256, Bue=512, Purple=242
+        }
+        static void Main(string[] args)
+        {
+            double test = 4.24;
+            Console.WriteLine(Math.Sqrt(test));
+            Console.WriteLine(Program.Square((int)test));
+
+            int test1 = 256;
+            switch ((Colors)test1)
+            {
+                case Colors.Green:
+                    Console.WriteLine("Color is green");
+                    break;
+                case Colors.Purple:
+                    Console.WriteLine("Color is green");
+                    break;
+                default:
+                    Console.WriteLine("Default");
+                    break;
+            }
+            //Using colons allows for specific value correction 
+            Program.TimeMethod(second: 43, hour: 12);
+        }
+
+        static double Square(int number)
+        {
+            return Math.Sqrt(number);
+        }
+
+        static void TimeMethod(int second=0, int minute=0, int hour = 0)
+        {
+
+        }
+    }
+}
